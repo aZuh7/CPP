5499e4fa2222f8642e7da12f345e46b66d0e0d48
aZuh7 <tyler.lopez15@icloud.com>
2021-10-05 22:41:19 -0500

    files from macbook

---
+++ Macbook/whale.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+
+  std::string input = "turpentine and turltes";
+  std::vector<char> vowels;
+
+  vowels.push_back('a');
+  vowels.push_back('e');
+  vowels.push_back('i');
+  vowels.push_back('o');
+  vowels.push_back('u');
+  
+  std::vector<char> result;
+
+  for (int i = 0; i < input.size(); i++) 
+  {
+    for (int j = 0; j < vowels.size(); j++) 
+    {
+      if (input[i] == vowels[j]) {
+        result.push_back(input[i]);
+      
+        if (input[i] == 'e' || input[i] == 'u') {
+          result.push_back(input[i]);
+        }
+      }
+
+    }
+  }
+  for (int k = 0; k < result.size(); k++) {
+    
+    std::cout << result[k];
+    
+  }
+}
